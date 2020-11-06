c80492cce56ea2a7ff650c60b3d877e2861d6085
raqeebchang <raqeebchang@gmail.com>
2020-11-05 21:53:39 -0800

    Added prototype slider to Sugar page

--- sugar.js
+++ sugar.js
@@ -1,3 +1,11 @@
 console.log(localStorage.option1);
 console.log(localStorage.option2);
 console.log(localStorage.option3);
+
+var slider = document.getElementById("slider");
+var values = document.getElementById("wrapper");
+values.style.bottom=slider.value*40+"px"
+slider.oninput=function(){
+    values.style.bottom=slider.value*40+"px"
+}
+
