b529462a4ed8ed71225d86171adf74478965d496
raqeebchang <raqeebchang@gmail.com>
2020-12-20 20:06:39 -0800

    Small additions to "ice" page

--- sugar.js
+++ sugar.js
@@ -6,6 +6,30 @@ var slider = document.getElementById("slider");
 var values = document.getElementById("wrapper");
 values.style.bottom=slider.value*40+"px"
 slider.oninput=function(){
-    values.style.bottom=slider.value*40+"px"
+    values.style.bottom=slider.value*40+"px";
+
 }
+
+function submission(){
+    localStorage.option4=slider.value*25;
+
+    if (document.getElementById("r1").checked){
+        localStorage.option5 = "No Ice";
+    }
+    else if (document.getElementById("r2").checked){
+        localStorage.option5 = "Less Ice";
+    }
+    else if (document.getElementById("r3").checked){
+        localStorage.option5 = "Regular Ice";
+    }
+    else if (document.getElementById("r4").checked){
+        localStorage.option5 = "More Ice";
+    }
+
+    console.log(localStorage.option4);
+    console.log(localStorage.option5);
+
+}
+//I can just have these as conditions for the submit, so it uses that state
+
 
